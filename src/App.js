3e0c7dda1fb39b521df2b5127476d4553845e100
Towobola <towobolak@gmail.com>
2024-09-23 16:08:02 +0100

    route commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import Dashboard from './Dashboard';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <div className='container'>
-
-        </div>
-        <div>
-        <Dashboard />
-        </div>
-      </div>
-    </Router>
+    <HashRouter>
+      <Routes>
+       
+          <Route path='/' element={ <Dashboard />} />
+        
+      </Routes>
+    </HashRouter>
   );
 }
 
